08d8eeb8ceef64cada42d68bd8f22339027180ac
Simbiat <simbiat@bk.ru>
2020-12-19 13:57:48 +0300

    Initial dummy release

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ArrayHelpers
- Set of useful functions to work with arrays
+Set of useful functions to work with arrays. Check comments near each function for details
---
+++ src/ArrayHelpers.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace ArrayHelpers;
+
+class Api
+{
+    public function dummy()
+    {
+        echo 'dummy release';
+    }
+}
+?>
