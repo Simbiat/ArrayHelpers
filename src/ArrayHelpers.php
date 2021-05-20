480dbe187736b04ba69e06ab43d81bccef4f38fb
Simbiat <simbiat@outlook.com>
2021-05-20 15:29:06 +0300

    Styling and minor adjustments

--- src/ArrayHelpers.php
+++ src/ArrayHelpers.php
@@ -17,95 +17,97 @@ class ArrayHelpers
         if ($rows <= 0 || count($array) < ($rows*2)) {
             $rows = intval(floor(count($array)/2));
         }
-        $newarray['top'] = array_slice($array, 0, $rows);
-        $newarray['bottom'] = array_reverse(array_slice($array, -$rows, $rows));
-        return $newarray;
+        $newArray['top'] = array_slice($array, 0, $rows);
+        $newArray['bottom'] = array_reverse(array_slice($array, -$rows, $rows));
+        return $newArray;
     }
-    
+
     #Useful to reduce number of travels to database. Instead of doing 2+ queries separately, we do just 1 query and then split it to several arrays in code
-    public function splitByKey(array $array, string $columnkey, array $newkeys, array $valuestocheck): array
+    public function splitByKey(array $array, string $columnKey, array $newKeys, array $valuesToCheck): array
     {
+        #Predefine empty array
+        $newArray = [];
         #Checking values
         if (empty($array)) {
             throw new \InvalidArgumentException('Empty array provided to splitByKey function.');
         }
-        if (empty($columnkey)) {
+        if (empty($columnKey)) {
             throw new \InvalidArgumentException('Empty key provided to splitByKey function.');
         }
-        if (empty($newkeys)) {
-            $newkeys = array_unique(array_column($array, $columnkey));
-            asort($newkeys, SORT_NATURAL);
+        if (empty($newKeys)) {
+            $newKeys = array_unique(array_column($array, $columnKey));
+            asort($newKeys, SORT_NATURAL);
         }
-        if (empty($valuestocheck)) {
-            $valuestocheck = $newkeys;
+        if (empty($valuesToCheck)) {
+            $valuesToCheck = $newKeys;
         }
         #Checking that length of both keys and values is same
-        if (count($newkeys) !== count($valuestocheck)) {
+        if (count($newKeys) !== count($valuesToCheck)) {
             throw new \UnexpectedValueException('List of keys and expected arrays provided to splitByKey function have different number of elements.');
         }
         #Prepare empty array
-        foreach ($newkeys as $arrkey=>$newkey) {
-            if (empty($newkey)) {
-                throw new \UnexpectedValueException('New key with index value \''.$arrkey.'\' is empty and cannot be used as key for new array by splitByKey function.');
+        foreach ($newKeys as $arrKey=>$newKey) {
+            if (empty($newKey)) {
+                throw new \UnexpectedValueException('New key with index value \''.$arrKey.'\' is empty and cannot be used as key for new array by splitByKey function.');
             } else {
-                if (is_string($newkey) || is_int($newkey)) {
-                    $newarray[$newkey] = [];
+                if (is_string($newKey) || is_int($newKey)) {
+                    $newArray[$newKey] = [];
                 } else {
-                    throw new \UnexpectedValueException('New key with index value \''.$arrkey.'\' is neither string nor integer and cannot be used as key for new array by splitByKey function.');
+                    throw new \UnexpectedValueException('New key with index value \''.$arrKey.'\' is neither string nor integer and cannot be used as key for new array by splitByKey function.');
                 }
             }
         }
         #Combine keys and values for easier identification, where a value should go to new array
-        $valuestocheck = array_combine($newkeys, $valuestocheck);
-        foreach ($valuestocheck as $key=>$value) {
+        $valuesToCheck = array_combine($newKeys, $valuesToCheck);
+        foreach ($valuesToCheck as $key=> $value) {
             foreach ($array as $item) {
                 #Value in item
-                if ($item[$columnkey] == $value) {
+                if ($item[$columnKey] == $value) {
                     #Remove column key, since it's not required after this
-                    unset($item[$columnkey]);
-                    $newarray[$key][] = $item;
+                    unset($item[$columnKey]);
+                    $newArray[$key][] = $item;
                 }
             }
         }
-        return $newarray;
+        return $newArray;
     }
-    
-    #Function allows to turn regular arrays (keyed 0, 1, 2, ... n) to assotiative one using values from the column provided as 2nd argument. Has option to remove that column from new arrays. Useful for structuring results from some complex SELECTs, when you know that each row returned is a separate entity
-    public function DigitToKey(array $oldarray, string $newkey, bool $keyunset = false): array
+
+    #Function allows to turn regular arrays (keyed 0, 1, 2, ... n) to associative one using values from the column provided as 2nd argument. Has option to remove that column from new arrays. Useful for structuring results from some complex SELECT, when you know that each row returned is a separate entity
+    public function DigitToKey(array $oldArray, string $newKey, bool $keyUnset = false): array
     {
-        if (empty($newkey)) {
+        if (empty($newKey)) {
             throw new \InvalidArgumentException('Empty key provided to DigitToKey function.');
         }
         #Setting the empty array as precaution
-        $newarray = [];
+        $newArray = [];
         #Iterrating the array provided
-        foreach ($oldarray as $oldkey=>$item) {
+        foreach ($oldArray as $item) {
             #Adding the element to new array
-            $newarray[$oldarray[$oldkey][$newkey]] = $oldarray[$oldkey];
+            $newArray[$item[$newKey]] = $item;
             #Removing old column
-            if ($keyunset == true) {
-                unset($newarray[$oldarray[$oldkey][$newkey]][$newkey]);
+            if ($keyUnset == true) {
+                unset($newArray[$item[$newKey]][$newKey]);
             }
         }
-        return $newarray;
+        return $newArray;
     }
-    
+
     #Function allows to turn multidimensional arrays to regular ones by "overwriting" each "row" with value from chosen column
-    public function MultiToSingle(array $oldarray, string $keytosave): array
+    public function MultiToSingle(array $oldArray, string $keyToSave): array
     {
-        if (empty($keytosave)) {
+        if (empty($keyToSave)) {
             throw new \InvalidArgumentException('Empty key provided to MultiToSingle function.');
         }
         #Setting the empty array as precaution
-        $newarray = [];
+        $newArray = [];
         #Iterrating the array provided
-        foreach ($oldarray as $oldkey=>$item) {
+        foreach ($oldArray as $oldKey=>$item) {
             #Adding the element to new array
-            $newarray[$oldkey] = $item[$keytosave];
+            $newArray[$oldKey] = $item[$keyToSave];
         }
-        return $newarray;
+        return $newArray;
     }
-    
+
     #Function converts set of selected columns' values to chosen type (INT by default). Initially created due to MySQL enforcing string values instead of integers in a lot of cases.
     public function ColumnsConversion(array $array, array|string $columns, string $type = 'int'): array
     {
@@ -131,36 +133,35 @@ class ArrayHelpers
                     'string' => (string)$value[$column],
                     'array' => (array)$value[$column],
                     'object' => (object)$value[$column],
-                    'uncset', 'null', 'nothing' => NULL,
                     default => NULL,
                 };
             }
         }
         return $array;
     }
-    
-    #Simple function, that removes all elements with certain value and optionally rekeys it (useful for indexed array, useless for assotiative ones)
-    public function RemoveByValue(array $array, mixed $remvalue, bool $strict = true, bool $rekey = false): array
+
+    #Simple function, that removes all elements with certain value and optionally re-keys it (useful for indexed array, useless for associative ones)
+    public function RemoveByValue(array $array, mixed $remValue, bool $strict = true, bool $reKey = false): array
     {
         #Iterrating the array provided
         foreach ($array as $key=>$value) {
             #Compare either strictly or not, depending on the flag provided
-            if (($strict === true && $array[$key] === $remvalue) || ($strict === false && $array[$key] == $remvalue)) {
+            if (($strict === true && $value === $remValue) || ($strict === false && $value == $remValue)) {
                 unset($array[$key]);
             }
         }
         #Rekey the array
-        if ($rekey === true) {
+        if ($reKey === true) {
             $array = array_values($array);
         }
         return $array;
     }
-    
-    #Function to sort a multidimensional array by values in column. Can be `reversed` to sort from larger to smaler (DESC order)
-    public function MultArrSort(array $array, string $column, bool $desc = false): array
+
+    #Function to sort a multidimensional array by values in column. Can be `reversed` to sort from larger to smaller (DESC order)
+    public function MultiArrSort(array $array, string $column, bool $desc = false): array
     {
         if (empty($column)) {
-            throw new \InvalidArgumentException('Empty key (column) provided to MultArrSort function.');
+            throw new \InvalidArgumentException('Empty key (column) provided to MultiArrSort function.');
         }
         if ($desc === true) {
             #Order in DESC
@@ -175,15 +176,17 @@ class ArrayHelpers
         }
         return $array;
     }
-    
+
     #Function to convert DBASE (.dbf) file to array
+    #Supressing inspection for functions related to DBase, since we have our own handler of lack of DBase extension
+    /** @noinspection PhpUndefinedFunctionInspection */
     public function dbfToArray(string $file): array
     {
         if (!file_exists($file)) {
             throw new \UnexpectedValueException('File \''.$file.'\' provided to dbfToArray function is not found.');
         }
         if (extension_loaded('dbase') === false) {
-            throw new \RuntimeException('dbase extension requried for dbfToArray function is not loaded.');
+            throw new \RuntimeException('dbase extension required for dbfToArray function is not loaded.');
         }
         #Setting the empty array as precaution
         $array = [];
@@ -208,4 +211,3 @@ class ArrayHelpers
     	return $array;
     }
 }
-?>
